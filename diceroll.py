61cf613ec7e7ff2992f21bfd6aa384567e73da33
Marc Galang <mgalang_h@dev.bse.edu>
2015-04-14 16:06:54 -0500

    no message

---
+++ diceroll.py
@@ -0,0 +1,10 @@
+masterdice=[4,6,8,10,12,20]
+dice1=[4,6,8,10,12,20]
+dice2=[4,6,8,10,12,20]
+#this is the number of dice (6) and how many sides are on each dice
+print dice1
+while any(dice1 != choice1)
+	choice1= raw_input("Player1 choose your die")
+print dice2
+while any(dice2 != choice1)
+	choice2= raw_input("Player2 choose your die")
